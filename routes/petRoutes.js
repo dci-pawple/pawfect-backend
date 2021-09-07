eae9e07c1dd65e710cd1d5f0617377ef4f50d168
ppphiiil <info@philmeinwelt.com>
2021-09-07 16:02:08 +0200

    add Route create Ad

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,17 +1,61 @@
-const express = require("express");
+const express = require('express')
 
-const PetModel = require("../models/petSchema");
+const PetModel = require('../models/petSchema')
 
-const Route = express.Router();
+const Route = express.Router()
 
-Route.get("/", async (req, res, next) => {
-  console.log("pets");
+var multer = require('multer')
+
+Route.get('/', async (req, res, next) => {
+  console.log('pets')
   try {
-    const pets = await PetModel.find({});
-    res.json({ succes: true, data: pets });
+    const pets = await PetModel.find({})
+    res.json({ succes: true, data: pets })
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-module.exports = Route;
+/**
+ * Route for new Ad
+ */
+
+let storage = multer.diskStorage({
+  destination: 'public/images/',
+  filename: function (req, file, cb) {
+    let picName =
+      file.originalname.split('.')[0] +
+      '-' +
+      Date.now() +
+      '.' +
+      file.mimetype.split('/')[1]
+    cb(null, picName)
+    req.picName = picName
+  }
+})
+
+let upload = multer({ storage: storage })
+
+Route.post('/newpet', upload.array('file', 10), function (req, res, next) {
+  try {
+    //contains the file
+    console.log('req.files', req.files)
+    //contains the text fields
+    console.log('req.body', req.body)
+
+    let pet = new PetModel({
+      name: req.body.name,
+      age: req.body.age
+      //images: req.files,
+    })
+
+    pet.save().then(result => {
+      res.send(result)
+    })
+    // res.send('respond with a resource');
+  } catch (err) {
+    console.log('Error in file upload Route =>', err)
+  }
+})
+
+module.exports = Route
